daaf6e9cf3a25b5c0becaf3bda085ecc039c5d2c
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-05-08 02:35:10 -0400

    adding sorting functions

--- insertion.js
+++ insertion.js
@@ -1,3 +1,21 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    // loop over the array starting from the second element
+    for (let i = 1; i < arr.length; i++) {
+        // loop backwards through the sorted part of the array
+        let j = i - 1;
+        // store the current element
+        let temp = arr[i];
+        // check if the element is smaller than the previous element
+        while (j >= 0 && temp < arr[j]) {
+            // shift the previous element to the right
+            arr[j + 1] = arr[j];
+            // decrement j
+            j--;
+        }
+        // insert the current element into the sorted part of the array
+        arr[j + 1] = temp;
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
--- selection.js
+++ selection.js
@@ -1,3 +1,22 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    // loop through the array starting from the first element
+    for (let i = 0; i < arr.length; i++) {
+        // set min to the index of the first element
+        let min = i;
+        // loop through the rest of the array
+        for (let j = i + 1; j < arr.length; j++) {
+            // if the current element is smaller than the minimum
+            if (arr[j] < arr[min]) {
+                // set min to the index of the current element
+                min = j;
+            }
+        }
+        // swap the minimum element with the first element
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
 
 module.exports = selectionSort;
