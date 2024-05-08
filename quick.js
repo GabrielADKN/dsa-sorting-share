f8e52cc98020f2cce6dcc66e0353fcebfbb9cef0
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-05-08 12:26:46 -0400

    quick sort algo

--- quick.js
+++ quick.js
@@ -3,12 +3,41 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1) {
+    // taking the first element as the pivot
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    //function to swap element in array
+    function swap(arr, i, j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    //Starting from the second element
+    for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        }
+    }
+    //swapping the pivot element
+    swap(arr, start, swapIdx);
+    return swapIdx;
+}
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
 
-module.exports = quickSort;
+module.exports = { pivot, quickSort };
